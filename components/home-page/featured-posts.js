836f76204b6828edb775cb1ed40255e46939e7c5
ryhanlon <ryhanlon@gmail.com>
2021-07-01 18:15:41 -0700

    re: add post-grid.js and post-item.js

--- components/home-page/featured-posts.js
+++ components/home-page/featured-posts.js
@@ -1,8 +1,13 @@
-import { styles } from './featured-posts.module.css';
+import classes from './featured-posts.module.css';
+
+import PostsGrid from "../posts/posts-grid";
 
 function FeaturedPosts(props) {
 	return (
-		<div>Featured Posts</div>
+		<section className={classes.latest}>
+			<h2>Featured Posts</h2>
+			<PostsGrid />
+		</section>
 	);
 }
 
---
+++ components/posts/post-item.js
@@ -0,0 +1,9 @@
+import classes from './post-item.module.css';
+
+function PostItem(props) {
+	return (
+		<div></div>
+	);
+}
+
+export default PostItem;
