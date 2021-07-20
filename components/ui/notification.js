05435378b7f98836c3988eb778775d93de34e58b
ryhanlon <ryhanlon@gmail.com>
2021-07-19 19:58:35 -0700

    re: add _document.js

--- components/ui/notification.js
+++ components/ui/notification.js
@@ -1,3 +1,5 @@
+import ReactDom from "react-dom";
+
 import classes from "./notification.module.css";
 
 function Notification(props) {
@@ -15,11 +17,12 @@ function Notification(props) {
 
   const cssClasses = `${classes.notification} ${statusClasses}`;
 
-  return (
+  return ReactDom.createPortal(
     <div className={cssClasses}>
       <h2>{title}</h2>
       <p>{message}</p>
-    </div>
+    </div>,
+    document.getElementById("notifications")
   );
 }
 
